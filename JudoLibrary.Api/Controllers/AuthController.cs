670b2da9200604f8525b451c041fe9cf8913c4f8
amkriskovic <antunmatijakriskovic@gmail.com>
2020-09-25 12:58:26 +0200

    Auth, Plugins

---
+++ JudoLibrary.Api/Controllers/AuthController.cs
@@ -0,0 +1,39 @@
+﻿using System.Threading.Tasks;
+using IdentityServer4.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JudoLibrary.Api.Controllers
+{
+    [ApiController]
+    [Route("api/auth")]
+    public class AuthController : ControllerBase
+    {
+        [HttpGet("logout")]
+        // Method for logging out User, logoutId for IS4 coz there can be multiple clients, OIDC will manage redirect & logout
+        // SignInManager DI at method level => used for signing out User
+        // IIdentityServerInteractionService DI at method level => used for discovering post logout redirect URI
+        public async Task<IActionResult> Logout(string logoutId,
+            [FromServices] SignInManager<IdentityUser> signInManager,
+            [FromServices] IIdentityServerInteractionService interactionService)
+        {
+            // # 1 Sign Out
+            // Signs the current user OUT of the application.
+            await signInManager.SignOutAsync();
+
+            // # 2 Where do we wanna resume after signing out
+            // Gets the logout context, need to provide logoutId
+            var logoutRequest = await interactionService.GetLogoutContextAsync(logoutId);
+            
+            // If logoutRequest -> (post logout redirect URI) is null or empty
+            if (string.IsNullOrEmpty(logoutRequest.PostLogoutRedirectUri))
+            {
+                // Return bad request
+                return BadRequest();
+            }
+
+            // If it's not null or empty, => Gets or sets the post logout redirect URI.
+            return Redirect(logoutRequest.PostLogoutRedirectUri);
+        }
+    }
+}
---
+++ web-client/store/auth.js
@@ -0,0 +1,58 @@
+﻿// Doing user init on client only -> user gonna get populated only when we return to the client
+
+// Local state
+const initState = () => ({
+  user: null,
+  loading: true // At start it's loading => process of initializing Auth
+})
+
+export const state = initState
+
+// Roles
+const ROLES = {
+  MODERATOR: "Mod"
+}
+
+export const getters = {
+  // If it's not loading && is user is not null => we are authenticated
+  authenticated: (state) => !state.loading && state.user != null,
+
+  // If User is authenticated first && he is moderator
+  moderator: (state, getters) => getters.authenticated && state.user.profile.role === ROLES.MODERATOR
+}
+
+export const mutations = {
+  saveUser(state, {user}) {
+    // Save user, to initial state user
+    state.user = user
+  },
+
+  // Authentication initialization has finished? => not loading anymore
+  finish(state) {
+    state.loading = false
+  }
+}
+
+export const actions = {
+  initialize({commit}) {
+    // Load the User object for the currently authenticated user
+    // * This is only get triggered if we are already authenticated and SIGNED IN
+    // * $auth is nuxt plugin - client-init.js -> injecting UserManager
+    return this.$auth.getUser()
+      .then(user => {
+        // If we have a user
+        if (user) {
+          console.log('User from local storage [already signed in] ', user)
+
+          // Commit user obj to saveUser mutation which will write to state => user is basically bih token
+          commit('saveUser', {user})
+
+          // * Setting the token to user that's in local storage
+          this.$axios.setToken(`Bearer ${user.access_token}`)
+        }
+      })
+      // Finally, doesn't matter what result is, commit finish -> stop loading
+      .finally(() => commit('finish'))
+
+  }
+}
